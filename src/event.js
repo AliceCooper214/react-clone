1101c4660f4f27316045c9514516e7df7b3472c1
cooper <openbilibili@126.com>
2023-08-24 13:18:45 +0800

    chore: event todo

--- src/event.js
+++ src/event.js
@@ -1,14 +1,46 @@
-import { updaterQueue } from './react/Component.js'
+import { flushUpdaterQueue, updaterQueue } from "./react/Component.js";
 
 export function addEvent(dom, eventName, bindFunction) {
-    dom.attach = dom.attach || {}
-    dom.attach[eventName] = bindFunction
+  dom.attach = dom.attach || {};
+  dom.attach[eventName] = bindFunction;
 
-    // 核心点 1/2: 事件绑定到document上
-    if (document[eventName]) return
-    document[eventName] = dispatchEvent;
+  // 核心点 1/2: 事件绑定到document上
+  if (document[eventName]) return;
+  document[eventName] = dispatchEvent;
+}
+/**
+ *
+ * @param {Event} nativeEvent
+ */
+function dispatchEvent(nativeEvent) {
+  updaterQueue.isBathingUpdate = true;
+
+  let syntheticEvent = createSyntheticEvent(nativeEvent);
+
+  let target = nativeEvent.target;
+
+  // 冒泡处理
+  while (target) {
+    syntheticEvent.currentTarget = target;
+    let bindFunction = target.attach && target.attach[eventName];
+    bindFunction && bindFunction(syntheticEvent);
+    if (syntheticEvent.isPropagationStopped) {
+      break;
+    }
+    target = target.parentNode;
+  }
+
+  flushUpdaterQueue();
 }
 
-function dispatchEvent(nativeEvent) {
-    updaterQueue.isBathingUpdate = true;
+function createSyntheticEvent(nativeEvent) {
+  let nativeEventKeyValues = {};
+  for (let key in nativeEvent) {
+    nativeEventKeyValues[key] =
+      typeof nativeEvent[key] === "function"
+        ? nativeEvent[key].bind(nativeEvent)
+        : nativeEvent[key];
+  }
+
+  return syntheticEvent;
 }
--- src/index.js
+++ src/index.js
@@ -36,28 +36,37 @@ class MyClassComponent extends React.Component {
 }
 
 class MyClassComponent1 extends React.Component {
-  counter = 0
+  counter = 0;
   constructor(props) {
     super(props);
-    this.state = { count: '0' };
+    this.state = { count: "0" };
   }
   updateShowText(newText) {
     this.setState({
-      count: newText
-    })
+      count: newText,
+    });
   }
   render() {
-    return <div className='test-class' style={
-      {
-        color: 'red',
-        cursor: 'pointer',
-        border: '1px solid gray',
-        borderRadius: '6px',
-        display: 'inline-block',
-        padding: '6px 12px'
-      }
-    } onClick={() => this.updateShowText('' + ++this.counter)}>Simple React Counter: {this.state.count}</div>
+    return (
+      <div
+        className="test-class"
+        style={{
+          color: "red",
+          cursor: "pointer",
+          border: "1px solid gray",
+          borderRadius: "6px",
+          display: "inline-block",
+          padding: "6px 12px",
+        }}
+        onClick={() => this.updateShowText("" + ++this.counter)}
+      >
+        Simple React Counter: {this.state.count}
+      </div>
+    );
   }
 }
 
-ReactDOM.render(<MyClassComponent1 xx="xx1" />, document.getElementById("root"));
+ReactDOM.render(
+  <MyClassComponent1 xx="xx1" />,
+  document.getElementById("root")
+);
