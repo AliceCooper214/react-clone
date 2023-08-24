f1eaa17d3473729c411ac39d3b4c9b694e106706
cooper <noemail@mail.com>
2023-08-24 18:10:53 +0800

    chore: diff todo

--- src/index.js
+++ src/index.js
@@ -103,52 +103,90 @@ import ReactDOM from "./react-dom";
 // }
 // }
 
-class CustomTextInput extends React.Component {
+// class CustomTextInput extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     // create a ref to store the textInput DOM element
+//     this.textInput = React.createRef();
+//     this.focusTextInput = this.focusTextInput.bind(this);
+//   }
+
+//   focusTextInput() {
+//     // Explicitly focus the text input using the raw DOM API
+//     // Note: we're accessing "current" to get the DOM node
+//     this.textInput.current.focus();
+//   }
+
+//   render() {
+//     // tell React that we want to associate the <input> ref
+//     // with the `textInput` that we created in the constructor
+//     return (
+//       <div>
+//         <input type="text" ref={this.textInput} />
+//         <input
+//           type="button"
+//           value="Focus the text input"
+//           onClick={this.focusTextInput}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+// class AutoFocusTextInput extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.textInput = React.createRef();
+//   }
+
+//   componentDidMount() {
+//     this.textInput.current.focusTextInput();
+//   }
+
+//   render() {
+//     return <CustomTextInput ref={this.textInput} />;
+//   }
+// }
+
+class MyClassComponent extends React.Component {
+  isReset = false;
+  oldArr = ["A", "B", "C", "D", "E"];
+  newArr = ["C", "B", "E", "F", "A"];
   constructor(props) {
     super(props);
-    // create a ref to store the textInput DOM element
-    this.textInput = React.createRef();
-    this.focusTextInput = this.focusTextInput.bind(this);
+    this.state = { arr: this.oldArr };
   }
-
-  focusTextInput() {
-    // Explicitly focus the text input using the raw DOM API
-    // Note: we're accessing "current" to get the DOM node
-    this.textInput.current.focus();
+  updateShowArr() {
+    this.setState({
+      arr: this.isReset ? this.oldArr : this.newArr,
+    });
+    this.isReset = !this.isReset;
   }
-
   render() {
-    // tell React that we want to associate the <input> ref
-    // with the `textInput` that we created in the constructor
     return (
       <div>
-        <input type="text" ref={this.textInput} />
-        <input
-          type="button"
-          value="Focus the text input"
-          onClick={this.focusTextInput}
-        />
+        <div
+          className="test-class"
+          style={{
+            color: "red",
+            cursor: "pointer",
+            border: "1px solid gray",
+            borderRadius: "6px",
+            display: "inline-block",
+            padding: "6px 12px",
+          }}
+          onClick={() => this.updateShowArr()}
+        >
+          Change The Text
+        </div>
+        <div>
+          {this.state.arr.map((item) => {
+            return <div key={item}>{item}</div>;
+          })}
+        </div>
       </div>
     );
   }
 }
 
-class AutoFocusTextInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.textInput = React.createRef();
-  }
-
-  componentDidMount() {
-    this.textInput.current.focusTextInput();
-  }
-
-  render() {
-    return <CustomTextInput ref={this.textInput} />;
-  }
-}
-
-ReactDOM.render(
-  <AutoFocusTextInput xx="xx1" />,
-  document.getElementById("root")
-);
+ReactDOM.render(<MyClassComponent xx="xx1" />, document.getElementById("root"));
--- src/react-dom.js
+++ src/react-dom.js
@@ -1,57 +1,122 @@
-import { REACT_ELEMENT, REACT_FORWARD_REF } from "./utils";
+import { REACT_ELEMENT, REACT_FORWARD_REF, REACT_TEXT } from "./utils";
 import { addEvent } from "./event";
 
 function render(VNode, containerDOM) {
   mount(VNode, containerDOM);
 }
 
-function mount(VNode, containerDOM) {
-  let newDOM = createDOM(VNode);
+function mount(vNode, containerDOM) {
+  let newDOM = createDOM(vNode);
   newDOM && containerDOM.appendChild(newDOM);
 }
 
-function createDOM(VNode) {
-  if (!VNode) return;
-  const { type, props, ref } = VNode;
+function createDOM(vNode) {
+  if (!vNode) return;
+  const { type, props, ref } = vNode;
   let dom;
+
   if (
     typeof type === "function" &&
     type.IS_CLASS_COMPONENT &&
-    VNode.$$typeof === REACT_ELEMENT
+    vNode.$$typeof === REACT_ELEMENT
   ) {
-    return getDomByClassComponent(VNode);
-  } else if (typeof type === "function" && VNode.$$typeof === REACT_ELEMENT) {
-    return getDomByFunctionComponent(VNode);
+    return getDomByClassComponent(vNode);
+  } else if (typeof type === "function" && vNode.$$typeof === REACT_ELEMENT) {
+    return getDomByFunctionComponent(vNode);
   } else if (type && type.$$typeof === REACT_FORWARD_REF) {
-    return getDomByRefForwardFunction(VNode);
-  } else if (type && VNode.$$typeof === REACT_ELEMENT) {
+    return getDomByRefForwardFunction(vNode);
+  } else if (type === REACT_TEXT) {
+    dom = document.createTextNode(props.text);
+  } else if (type && vNode.$$typeof === REACT_ELEMENT) {
     dom = document.createElement(type);
   }
+
   if (props) {
     if (typeof props.children === "object" && props.children.type) {
       mount(props.children, dom);
     } else if (Array.isArray(props.children)) {
       mountArray(props.children, dom);
-    } else if (typeof props.children === "string") {
-      dom.appendChild(document.createTextNode(props.children));
     }
+    // else if (typeof props.children === "string") {
+    //   dom.appendChild(document.createTextNode(props.children));
+    // }
   }
   setPropsForDOM(dom, props);
-  VNode.dom = dom;
+  vNode.dom = dom;
   ref && (ref.current = dom);
   return dom;
 }
 
-export function updateDomTree(oldDOM, newVNode) {
-  if (!oldDOM) return;
-  let parentNode = oldDOM.parentNode;
-  parentNode.removeChild(oldDOM);
-  parentNode.appendChild(createDOM(newVNode));
+export function updateDomTree(oldVNode, newVNode, oldDOM) {
+  const typeMap = {
+    NO_OPERATE: !oldVNode && !newVNode,
+    ADD: !oldVNode && newVNode,
+    DELETE: oldVNode && !newVNode,
+    REPLACE: oldVNode && newVNode && oldVNode.type !== newVNode.type,
+  };
+  let UPDATE_TYPE = Object.keys(typeMap).filter((key) => typeMap[key])[0];
+
+  switch (UPDATE_TYPE) {
+    case "NO_OPERATE":
+      break;
+    case "DELETE":
+      removeVNode(oldVNode);
+      break;
+    case "ADD":
+      oldDOM.parentNode.appendChild(createDOM(newVNode));
+      break;
+    case "REPLACE":
+      removeVNode(oldVNode);
+      oldDOM.parentNode.appendChild(createDOM(newVNode));
+      break;
+    default:
+      deepDOMDiff(oldVNode, newVNode);
+      break;
+  }
+}
+
+function updateChildren(parentDOM, oldVNodeChildren, newVNodeChildren) {}
+
+function deepDOMDiff(oldVNode, newVNode) {
+  let diffTypeMap = {
+    ORIGIN_NODE: typeof oldVNode.type === "string",
+    CLASS_COMPONENT:
+      typeof oldVNode.type === "function" && oldVNode.type.isReactComponent,
+    FUNCTION_COMPONENT: typeof oldVNode.type === "function",
+    TEXT: oldVNode.type === REACT_TEXT,
+  };
+  let DIFF_TYPE = Object.keys(diffTypeMap).filter((key) => diffTypeMap[key])[0];
+  switch (DIFF_TYPE) {
+    case "ORIGIN_NODE":
+      let currentDOM = (newVNode.dom = findDomByVNode(oldVNode));
+      setPropsForDOM(currentDOM, newVNode.props);
+      updateChildren(
+        currentDOM,
+        oldVNode.props.children,
+        newVNode.props.children
+      );
+      break;
+    case "CLASS_COMPONENT":
+      break;
+    case "FUNCTION_COMPONENT":
+      break;
+    case "TEXT":
+      newVNode.dom = findDomByVNode(newVNode);
+      newVNode.dom.textContent = newVNode.props.text;
+      break;
+    default:
+      break;
+  }
 }
 
 export function findDomByVNode(VNode) {
   if (!VNode) return;
   if (VNode.dom) return VNode.dom;
+}
+
+function removeVNode(vNode) {
+  const currentDOM = findDomByVNode(vNode);
+  if (currentDOM) currentDOM.remove();
 }
 
 function getDomByRefForwardFunction(vNode) {
@@ -97,11 +162,13 @@ function setPropsForDOM(dom, VNodeProps = {}) {
 function mountArray(children, parent) {
   if (!Array.isArray(children)) return;
   for (let i = 0; i < children.length; i++) {
-    if (typeof children[i] === "string") {
-      parent.appendChild(document.createTextNode(children[i]));
-    } else {
-      mount(children[i], parent);
-    }
+    // if (typeof children[i] === "string") {
+    //   parent.appendChild(document.createTextNode(children[i]));
+    // } else {
+    //   mount(children[i], parent);
+    // }
+    children[i].index = i;
+    mount(children[i], parent);
   }
 }
 
